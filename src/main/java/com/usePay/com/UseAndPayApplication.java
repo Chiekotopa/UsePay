8158801f3833db4709e8a9a343f05b3e3f96ecc3
chiek <chiek@DESKTOP-7B8P5FA>
2021-12-28 16:01:05 +0100

    new fonctionnality

---
+++ src/main/java/com/usePay/com/UseAndPayApplication.java
@@ -0,0 +1,13 @@
+package com.usePay.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseAndPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseAndPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usePay/com/UseAndPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usePay.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseAndPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
