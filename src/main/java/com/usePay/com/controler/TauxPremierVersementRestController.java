e28c767f02305f4d2ef242ec16ad91f69776e83e
chiek <chiek@DESKTOP-7B8P5FA>
2022-02-14 17:36:19 +0100

    add getlistTauxversement

--- src/main/java/com/usePay/com/controler/TauxPremierVersementRestController.java
+++ src/main/java/com/usePay/com/controler/TauxPremierVersementRestController.java
@@ -7,9 +7,12 @@ package com.usePay.com.controler;
 
 import com.usePay.com.Dao.TauxPremierVersementRepository;
 import com.usePay.com.entities.TauxPremierVerssement;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,26 +26,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/apiTauxVersement")
 @CrossOrigin(origins = "*")
 public class TauxPremierVersementRestController {
-    
-    @Autowired 
+
+    @Autowired
     TauxPremierVersementRepository versementRepository;
-    
-     @PostMapping(value = "SaveTauxVersement")
-    public Object SavePeriode(@RequestBody TauxPremierVerssement premierVerssement){
-        HashMap map=new HashMap();
-        
+
+    @PostMapping(value = "SaveTauxVersement")
+    public Object SavePeriode(@RequestBody TauxPremierVerssement premierVerssement) {
+        HashMap map = new HashMap();
+
         try {
             versementRepository.save(premierVerssement);
             map.put("status", "1");
             map.put("message", "Success");
             return map;
-            
+
         } catch (Exception e) {
-           
+
             map.put("status", "0");
             map.put("message", e.getMessage());
             return map;
         }
     }
-    
+
+    @GetMapping(value = "getListTauxVersement")
+    public Object getListTauxVersement(@RequestBody TauxPremierVerssement premierVerssement) {
+        HashMap map = new HashMap();
+        try {
+
+            return versementRepository.findAll();
+
+        } catch (Exception e) {
+
+            map.put("status", "0");
+            map.put("message", e.getMessage());
+            return map;
+        }
+    }
+
 }
